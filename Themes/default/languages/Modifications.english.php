3234e82f5fd30a2fb82377bd5b43ef849fb41114
SMFPortal <info@smf-portal.hu>
2013-03-21 09:21:19 +0100

    Create Modifications.english.php

---
+++ Themes/default/languages/Modifications.english.php
@@ -0,0 +1,39 @@
+<?php
+// Version: 2.0; Modifications
+
+$txt['permissiongroup_tracker'] = 'Tracker';
+$txt['permissiongroup_simple_tracker'] = 'Use Tracker';
+$txt['permissionname_tracker_view'] = 'View Tracker';
+$txt['permissionname_tracker_download'] = 'Download';
+$txt['permissionname_tracker_delete'] = 'Delete Torrents';
+$txt['permissionname_tracker_delete_own'] = 'Own';
+$txt['permissionname_tracker_delete_any'] = 'Any';
+$txt['permissionname_tracker_upload'] = 'Upload Torrents';
+$txt['permissionname_tracker_admin'] = 'Admin';
+$txt['permissionname_tracker_view_stats'] = 'View Statistics';
+$txt['permissionname_tracker_view_stats_own'] = 'Own';
+$txt['permissionname_tracker_view_stats_any'] = 'Any';
+
+$txt['cannot_tracker_view'] = 'You are not allowed to view tracker';
+$txt['cannot_tracker_delete_own'] = 'You are not allowed to delete torrents';
+$txt['cannot_tracker_download'] = 'You are not allowed to download from tracker';
+
+$txt['tracker_admin'] = 'Tracker Admin';
+$txt['tracker_settings'] = 'Tracker Settings';
+
+$txt['trackerStats'] = 'Tracker Statistics';
+
+$txt['torrent_ratio'] = 'Ratio';
+$txt['torrent_downloaded'] = 'Downloaded';
+$txt['torrent_uploaded'] = 'Uploaded';
+$txt['active_torrents'] = 'Active Torrents';
+$txt['active_downloads'] = 'Downloads';
+$txt['active_uploads'] = 'Uploads';
+
+$txt['torrent_ib_seeds'] = 'seeders';
+$txt['torrent_ib_leechs'] = 'leechers';
+$txt['torrent_ib_transfer'] = 'transfered';
+$txt['torrent_ib_downloads'] = 'downloads';
+$txt['torrent_ib_details'] = 'view details';
+
+?>
